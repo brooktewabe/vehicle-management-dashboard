57e0b85c9ff700d00f776818e21590b922103f14
brooktewabe <101641521+brooktewabe@users.noreply.github.com>
2024-12-13 13:12:18 -0500

    Update env

--- Node backend/config/db.js
+++ Node backend/config/db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-
+require('dotenv').config();
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/vehicleDB');
+    await mongoose.connect(process.env.MONGO_URL);
     console.log('MongoDB connected successfully');
   } catch (error) {
     console.error('MongoDB connection error:', error);
